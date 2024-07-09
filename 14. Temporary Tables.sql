dafb0f7eead96c1d031bdf222af69e583f02b332
Chase Di Maria Breisinger <146398672+Lqvy@users.noreply.github.com>
2024-07-09 11:33:57 -0400

    14. Temporary Tables.sql
    Updated Notes.

--- 14. Temporary Tables.sql
+++ 14. Temporary Tables.sql
@@ -1,17 +1,21 @@
 -- Temporary Tables
 
+-- Creating the temp tables rows and columns
 create temporary table temp_table
 (first_name varchar (50),
 last_name varchar (50),
 favorite_food varchar (50)
 );
 
+-- Populating the temp table
 insert into temp_table
 values('Chase','Breisinger','Hot Wings');
 
+-- Viewing
 select * 
 from temp_table;
 
+-- Temp table from an existing table
 create temporary table salary_over_50k
 select *
 from employee_salary
